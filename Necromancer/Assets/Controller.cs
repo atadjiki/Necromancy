37b48ca79bd096001a44bdf3915ed5bb3a50b3dc
Arash Tadjiki <atadjiki@gmail.com>
2018-09-16 18:35:58 -0600

    Added detection for minions, can possess them if close enough

--- Necromancer/Assets/Controller.cs
+++ Necromancer/Assets/Controller.cs
@@ -6,6 +6,7 @@ public class Controller : MonoBehaviour {
 
     public float speed;
 
+    public GameObject necromancer;
     public List<GameObject> targets;
     private GameObject current;
 
@@ -18,28 +19,87 @@ public class Controller : MonoBehaviour {
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             Vector3 position = current.transform.position;
-            position.x--;
+            position.x-=speed;
             current.transform.position = position;
-        } else if (Input.GetKeyDown(KeyCode.RightArrow)){
+
+            if(current != necromancer){
+                position = necromancer.transform.position;
+                position.x -= speed/5;
+                necromancer.transform.position = position;
+            }
+        } else if (Input.GetKey(KeyCode.RightArrow)){
 
             Vector3 position = current.transform.position;
-            position.x++;
+            position.x += speed;
             current.transform.position = position;
+            if (current != necromancer)
+            {
+                position = necromancer.transform.position;
+                position.x += speed/5;
+                necromancer.transform.position = position;
+            }
         }
 
-        if(Input.GetKey(KeyCode.Alpha1)){switchTarget(0);}
-        if(Input.GetKey(KeyCode.Alpha2)){switchTarget(1);}
-        if(Input.GetKey(KeyCode.Alpha3)){switchTarget(2);}
+        if (Input.GetKey(KeyCode.Alpha1)){switchNecromancer();}
+        if(Input.GetKey(KeyCode.Alpha2)){switchTarget(0);}
+        if(Input.GetKey(KeyCode.Alpha3)){switchTarget(1);}
+
+
+        //check nearby minion candidates
+        if(current.GetComponentInChildren<DetectNearby>() != null){
+            bool colliding = current.GetComponentInChildren<DetectNearby>().getCollided();
+
+            //if there is a nearby minion to be added
+            if (current.GetComponentInChildren<DetectNearby>().getCollidedObject() != null)
+            {
+                GameObject collidedObj = current.GetComponentInChildren<DetectNearby>().getCollidedObject();
+                if (colliding && collidedObj != null)
+                {
+                    if(collidedObj.gameObject.tag != "" && collidedObj.gameObject.tag == "DetectionRadius"){
+                        setDetectColor(collidedObj);
+                        if (Input.GetKey(KeyCode.Space))
+                        {
+                            //add the minion to our collection 
+                            targets.Add(collidedObj);
+                            collidedObj.gameObject.tag = "Untagged";
+                            //now change control to new object
+                            Debug.Log("New Target");
+                            current = targets[targets.Count - 1];
+                            setColor();
+                        }
+                    }
+
+                }
+                else if (!colliding && collidedObj != null)
+                {
+                    resetDetectColor(collidedObj);
+                }
+            }
+        }
+
+
 
     }
 
     public void switchTarget(int index){
-        Debug.Log("Switching to Target: " + (index + 1));
-        current = targets[index];
+
+        if(index <= targets.Count){
+            Debug.Log("Switching to Target: " + (index + 1));
+            current = targets[index];
+            setColor();
+        }
+
+    }
+
+    public void switchNecromancer()
+    {
+
+        current = necromancer;
         setColor();
+
     }
 
     private void setColor(){
@@ -49,8 +109,19 @@ public class Controller : MonoBehaviour {
             obj.GetComponent<Renderer>().material.color = Color.white;
         }
 
+        necromancer.GetComponent<Renderer>().material.color = Color.white;
+
         //Fetch the Renderer from the GameObject
         current.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    private void setDetectColor(GameObject obj){
+        obj.GetComponentInParent<Renderer>().material.color = Color.yellow;
+    }
+
+    private void resetDetectColor(GameObject obj)
+    {
+        obj.GetComponentInParent<Renderer>().material.color = Color.white;
     }
 
 
---
+++ Necromancer/Assets/DetectNearby.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectNearby : MonoBehaviour {
+
+    GameObject lastCollidedObject;
+    bool collided;
+
+	// Use this for initialization
+	void Start () {
+        collided = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Object detected: " + other.gameObject.name);
+        lastCollidedObject = other.gameObject;
+        collided = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Object escaped: " + other.gameObject.name);
+        collided = false;
+    }
+
+    public bool getCollided(){
+        return collided;
+    }
+
+    public GameObject getCollidedObject(){
+        return lastCollidedObject;
+    }
+}
